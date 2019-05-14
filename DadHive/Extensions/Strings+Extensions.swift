b03c76dbe675272593642b80db3651fad65d3b84
Michael Westbrooks II <michael.westbrooks@redroostertec.com>
2019-05-14 18:59:47 -0400

    Added additional extensions.

--- DadHive/Extensions/Strings+Extensions.swift
+++ DadHive/Extensions/Strings+Extensions.swift
@@ -207,4 +207,111 @@ extension NSString {
 //    return key
 //}
 
-
+extension String {
+    var html2Attributed: NSAttributedString? {
+        do {
+            guard let data = data(using: String.Encoding.utf8) else {
+                return nil
+            }
+            return try NSAttributedString(data: data,
+                                          options: [.documentType: NSAttributedString.DocumentType.html,
+                                                    .characterEncoding: String.Encoding.utf8.rawValue],
+                                          documentAttributes: nil)
+        } catch {
+            print("error: ", error)
+            return nil
+        }
+    }
+    
+    var htmlAttributed: (NSAttributedString?, NSDictionary?) {
+        do {
+            guard let data = data(using: String.Encoding.utf8) else {
+                return (nil, nil)
+            }
+            
+            var dict:NSDictionary?
+            dict = NSMutableDictionary()
+            
+            return try (NSAttributedString(data: data,
+                                           options: [.documentType: NSAttributedString.DocumentType.html,
+                                                     .characterEncoding: String.Encoding.utf8.rawValue],
+                                           documentAttributes: &dict), dict)
+        } catch {
+            print("error: ", error)
+            return (nil, nil)
+        }
+    }
+    
+    func htmlAttributed(using font: UIFont, color: UIColor) -> NSAttributedString? {
+        do {
+            let htmlCSSString = "<style>" +
+                "html *" +
+                "{" +
+                "font-size: \(font.pointSize)pt !important;" +
+                "color: #\(color.hexValue()) !important;" +
+                "font-family: \(font.familyName), Helvetica !important;" +
+            "}</style> \(self)"
+            
+            guard let data = htmlCSSString.data(using: String.Encoding.utf8) else {
+                return nil
+            }
+            
+            return try NSAttributedString(data: data,
+                                          options: [.documentType: NSAttributedString.DocumentType.html,
+                                                    .characterEncoding: String.Encoding.utf8.rawValue],
+                                          documentAttributes: nil)
+        } catch {
+            print("error: ", error)
+            return nil
+        }
+    }
+    
+    func htmlAttributed(family: String?, size: CGFloat, color: UIColor) -> NSAttributedString? {
+        do {
+            let htmlCSSString = "<style>" +
+                "html *" +
+                "{" +
+                "font-size: \(size)pt !important;" +
+                "color: #\(color.hexValue()) !important;" +
+                "font-family: \(family ?? "Helvetica"), Helvetica !important;" +
+            "}</style> \(self)"
+            
+            guard let data = htmlCSSString.data(using: String.Encoding.utf8) else {
+                return nil
+            }
+            
+            return try NSAttributedString(data: data,
+                                          options: [.documentType: NSAttributedString.DocumentType.html,
+                                                    .characterEncoding: String.Encoding.utf8.rawValue],
+                                          documentAttributes: nil)
+        } catch {
+            print("error: ", error)
+            return nil
+        }
+    }
+    
+    func convertHtml() -> NSAttributedString {
+        
+        guard let data = data(using: .utf8) else {
+            
+            return NSAttributedString()
+            
+        }
+        
+        do {
+            
+            return try NSAttributedString(data: data,
+                                          
+                                          options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue],
+                                          
+                                          documentAttributes: nil)
+            
+        } catch {
+            
+            return NSAttributedString()
+            
+        }
+        
+    }
+    
+}
